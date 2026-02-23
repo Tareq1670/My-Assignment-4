f5cbd5c5c32d0e29ebd0ee797832bc076f395198
Tareq <tariqulislamtareq1670@gmail.com>
2026-02-23 11:16:18 +0600

    In this time I create counter section

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-// Names
+//Button Names
 const allBtn = document.getElementById("all_btn");
 const interviewBtn = document.getElementById("interview_btn");
 const rejectedBtn = document.getElementById("rejected_btn");
 
+// Counter Names
+const totalCount = document.getElementById("total_count");
+const interviewCount = document.getElementById("interview_count");
+const rejectedCount = document.getElementById("rejected_count");
+
+
+// Containers
+const mainContainer = document.getElementById("main_container");
+const interviewContainer = document.getElementById("interview_container");
+const rejectedContainer = document.getElementById("rejected_container");
+
+
+// Containers Variables
+let interviewItems = [];
+let rejectedItems = [];
+let selectedCategory = "all_btn";
+
 
 document.addEventListener("DOMContentLoaded",()=>{
     toggleButton("all_btn")
+
 })
 // Toggle Button Create
 function toggleButton(id){
@@ -20,3 +38,12 @@ function toggleButton(id){
     const selected = document.getElementById(id);
     selected.classList.add("bg-[#3b82f6]", "text-white","transition-all","duration-300");
 }
+
+
+// Counter Section 
+function itemsCounter(){
+    totalCount.textContent = mainContainer.children.length;
+    rejectedCount.textContent = rejectedItems.length;
+    interviewCount.textContent = interviewItems.length;
+}
+itemsCounter();
